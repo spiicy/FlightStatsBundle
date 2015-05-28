9dcaa5bfa36434c96407d00da067b7913e2c53bf
eveyrat <emmanuel@unitedworldwrestling.org>
2015-05-28 19:34:19 +0000

    Initial commit

---
+++ UnitedWorldWrestlingFlightStatsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace UnitedWorldWrestling\Bundle\FlightStatsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UnitedWorldWrestlingFlightStatsBundle extends Bundle
+{
+}
